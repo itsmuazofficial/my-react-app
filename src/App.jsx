adc357cb3ce40e595e8bc5cdaa91638176419935
itsmuazofficial <muazisrar333@gmail.com>
2026-06-28 23:03:31 +0500

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 // import About from "./components/About";
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
